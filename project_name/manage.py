8808fa9cfff4288846aacc4b18501521cbe26f77
Laurent Meunier <laurent@deltalima.net>
2013-11-10 14:37:18 +0100

    Remove unneeded files
    The `master` branch contains only the README.md file (how to use this
    template). Real files needed by the template are in different branches
    (one branch for each Django version).

--- project_name/manage.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
-    from django.core.management import execute_from_command_line
-    execute_from_command_line(sys.argv)
